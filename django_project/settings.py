7b7894dd2f8e1f13e3b009abee245c3bfd4ef26e
Ryan Lewis <r2c2ryan@gmail.com>
2021-01-07 18:59:55 -0800

    cast base_dir as str

--- django_project/settings.py
+++ django_project/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
